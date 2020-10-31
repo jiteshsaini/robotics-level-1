7c314587b585f57e5768a1595b31c8decde8abfa
jiteshsaini <46092054+jiteshsaini@users.noreply.github.com>
2020-10-31 14:19:32 +0530

    Update remote.php

--- earthrover/remote.php
+++ earthrover/remote.php
@@ -7,10 +7,11 @@ Created by: Jitesh Saini
    <title>Remote</title>  
    <link href="css/remote.css" rel="stylesheet" type="text/css">  
    <script src="js/jquery.min.js"></script>  
-   <script src="js/earthrover.js"></script>              
+   <script src="js/remote.js"></script>  
+             
 </head> 
 
-<body onload="set_speed(50)">
+<body>
 <?php
 include_once 'vars.php';
 
@@ -34,39 +35,28 @@ gpio_initialise(); // initialising the GPIO pins
 	</div>
 	<!-- ================================================================================= -->
 	
-	<br />
-	<!-- =================Speed Buttons=================================================== -->
+	<br><br>
 	
+	<!-- =============Range Slider for speed==============================================-->
 	<div class='box_row'>
-		<div class='box_1'>
-			<txt>Speed</txt>
-			<input class="button_1" style="float:left" type="submit" onclick="button_speed('-');" value="-"/>
-			<b id= 'disp_speed' style='color:blue'>50</b>					
-			<input  class="button_1" style="float:right" type="submit" onclick="button_speed('+');" value="+"/>
+	
+		<div class="slidecontainer">
+			<input type="range" min="0" max="100" value="50" class="slider" id="myRange" step="5">
+			<p>Speed: <span id="speed">50</span></p>
 		</div>
-		<div style='float:right' class='box_1'>
-			<input id= 'txt_send' style='width:40%;font-size:20px' type='text' name='t2'>			
-			<input id="cmd_send" type="submit" value="Set Speed Value" onclick="button_send();"/>
-		</div>
+		<script>
+			var slider = document.getElementById("myRange");
+			var output = document.getElementById("speed");
+			output.innerHTML = slider.value;
+			speed_slider(slider.value);
+			slider.oninput = function() {
+				output.innerHTML = this.value;
+				speed_slider(this.value);
+				}
+		</script>
 	</div>
 	<!-- ================================================================================== -->
-	
 </div>
-
-<!-- ====================================================================================== -->
-<div >
-		<div class='box_1'>
-			<b  style='color:green;display:none' id='disp_local_dir'>dir sent to server</b>
-			<br />			
-			<b  style='color:green;display:none' id='disp_local_sp'>sp sent to server</b>
-				
-		</div>
-		
-		<div class='box_1'>
-			<b  style='color:red;display:none' id='disp_server'>Response from server</b>
-		</div>
-</div>
-<!-- ========================================================================================== -->
 
 </body>
 </html>
