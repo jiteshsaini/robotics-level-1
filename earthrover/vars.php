2508abb31f470174c8b6cb477a7e20181713b81d
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2021-06-26 11:19:03 +0530

    Update vars.php

--- earthrover/vars.php
+++ earthrover/vars.php
@@ -14,60 +14,78 @@ function gpio_initialise(){
 	//echo"init<br>";
 	global $m1_1,$m1_2,$m2_1,$m2_2;
 
-	system("gpio -g mode $m1_1 out");
-	system("gpio -g mode $m1_2 out");
-	system("gpio -g mode $m2_1 out");
-	system("gpio -g mode $m2_2 out");
-	system("gpio -g write $m1_1 0");
-	system("gpio -g write $m1_2 0");
-	system("gpio -g write $m2_1 0");
-	system("gpio -g write $m2_2 0");
+	//====motors=================
+	
+	set_gpio($m1_1,'output');
+	set_gpio($m1_2,'output');
+	set_gpio($m2_1,'output');
+	set_gpio($m2_2,'output');
+	
+	set_gpio($m1_1,'0');
+	set_gpio($m1_2,'0');
+	set_gpio($m2_1,'0');
+	set_gpio($m2_2,'0');
+
 }
 
 function move($dir){
 	switch ($dir) {	
 		case 'f': forward();	break;
-		case 'b': back();		break;
-		case 'r': right();		break;
-		case 'l': left();		break;
-		case 's': stop();		break;	
+		case 'b': back();	break;
+		case 'r': right();	break;
+		case 'l': left();	break;
+		case 's': stop();	break;	
 	}
 }
 
 function right(){
 	global $m1_1,$m1_2,$m2_1,$m2_2; 
-	system("gpio -g write $m1_1 1");
-	system("gpio -g write $m1_2 0");
-	system("gpio -g write $m2_1 1");
-	system("gpio -g write $m2_2 0");
+	set_gpio($m1_1,'1');
+	set_gpio($m1_2,'0');
+	set_gpio($m2_1,'1');
+	set_gpio($m2_2,'0');
+	
 }
 function left(){
 	global $m1_1,$m1_2,$m2_1,$m2_2; 
-	system("gpio -g write $m1_1 0");
-	system("gpio -g write $m1_2 1");
-	system("gpio -g write $m2_1 0");
-	system("gpio -g write $m2_2 1");
+	set_gpio($m1_1,'0');
+	set_gpio($m1_2,'1');
+	set_gpio($m2_1,'0');
+	set_gpio($m2_2,'1');
 }
 function forward(){
 	global $m1_1,$m1_2,$m2_1,$m2_2; 
-	system("gpio -g write $m1_1 1");
-	system("gpio -g write $m1_2 0");
-	system("gpio -g write $m2_1 0");
-	system("gpio -g write $m2_2 1");
+	set_gpio($m1_1,'1');
+	set_gpio($m1_2,'0');
+	set_gpio($m2_1,'0');
+	set_gpio($m2_2,'1');
+	//echo"fwd<br>";
 }
 function back(){
 	global $m1_1,$m1_2,$m2_1,$m2_2; 
-	system("gpio -g write $m1_1 0");
-	system("gpio -g write $m1_2 1");
-	system("gpio -g write $m2_1 1");
-	system("gpio -g write $m2_2 0");
+	set_gpio($m1_1,'0');
+	set_gpio($m1_2,'1');
+	set_gpio($m2_1,'1');
+	set_gpio($m2_2,'0');
 }
 function stop(){
 	global $m1_1,$m1_2,$m2_1,$m2_2; 
-	system("gpio -g write $m1_1 1");
-	system("gpio -g write $m1_2 1");
-	system("gpio -g write $m2_1 1");
-	system("gpio -g write $m2_2 1");
+	set_gpio($m1_1,'0');
+	set_gpio($m1_2,'0');
+	set_gpio($m2_1,'0');
+	set_gpio($m2_2,'0');
 }
+
+function set_gpio($pin,$x){
+	switch($x){
+		case '1': $z='dh';break;
+		case '0': $z='dl';break;
+		case 'output': $z='op';break;
+	}
+	$cmd="sudo raspi-gpio set $pin $z";
+	system($cmd);
+	//echo"$x: $cmd <br>";
+}
+
 ?>
 
